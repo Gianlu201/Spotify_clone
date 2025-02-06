e8f38322449644d46e7b5482ef128e40f0510526
Gianlu201 <gianlucadidiego2001@gmail.com>
2025-02-06 22:10:54 +0100

    volume slider done

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -5,6 +5,10 @@ import {
   BsShuffle,
   BsSkipEndFill,
   BsSkipStartFill,
+  BsVolumeDownFill,
+  BsVolumeMuteFill,
+  BsVolumeOffFill,
+  BsVolumeUpFill,
 } from 'react-icons/bs';
 import { useAppSelector } from '../redux/app/hooks';
 import { useEffect, useState } from 'react';
@@ -12,6 +16,9 @@ import { Track } from '../types/types';
 
 export default function Player() {
   const [reproductionState, setReproductionState] = useState(false);
+  const [volumeLevel, setVolumeLevel] = useState(30);
+  const [volumeMute, setVolumeMute] = useState(false);
+  const [lastVolumeValue, setLastVolumeValue] = useState<number>(30);
 
   const currentSong = useAppSelector((store) => store.currentSong) as Track;
 
@@ -33,8 +40,13 @@ export default function Player() {
     }
   }, [currentSong]);
 
+  useEffect(() => {
+    const sound = document.getElementById('audio');
+    sound.volume = volumeLevel / 100;
+  }, [volumeLevel]);
+
   return (
-    <div className='h-[10vh] max-h-[70px] fixed start-0 bottom-0 z-50 w-full bg-black flex justify-between items-center'>
+    <div className='h-[10vh] max-h-[70px] fixed start-0 bottom-0 z-50 w-full bg-black flex justify-between items-center px-5'>
       <div className='flex text-white'>
         {currentSong.id !== 0 && (
           <>
@@ -74,12 +86,60 @@ export default function Player() {
         </div>
         <div>
           <audio src={currentSong.preview} id='audio' autoPlay />
+          <input type='range' name='player' />
         </div>
       </div>
 
-      <div>
-        <button></button>
-        <input type='range' name='volume' min={0} max={100} />
+      <div className='flex items-center'>
+        <button
+          className='cursor-pointer'
+          onClick={() => {
+            if (volumeMute) {
+              setVolumeLevel(lastVolumeValue);
+              setVolumeMute(false);
+            } else {
+              setLastVolumeValue(volumeLevel);
+              setVolumeMute(true);
+              setVolumeLevel(0);
+            }
+          }}
+        >
+          {volumeMute ? (
+            <BsVolumeMuteFill className='text-white text-2xl' />
+          ) : (
+            <>
+              <BsVolumeOffFill
+                className={'text-white text-2xl '.concat(
+                  volumeLevel === 0 ? '' : 'hidden'
+                )}
+              />
+              <BsVolumeDownFill
+                className={'text-white text-2xl '.concat(
+                  volumeLevel > 0 && volumeLevel <= 50 ? '' : 'hidden'
+                )}
+              />
+              <BsVolumeUpFill
+                className={'text-white text-2xl '.concat(
+                  volumeLevel > 50 ? '' : 'hidden'
+                )}
+              />
+            </>
+          )}
+        </button>
+        <input
+          type='range'
+          name='volume'
+          value={volumeLevel}
+          onChange={(e) => {
+            setVolumeLevel(parseInt(e.target.value));
+          }}
+          min={0}
+          max={100}
+          disabled={volumeMute ? true : false}
+          className={''.concat(
+            volumeMute ? 'cursor-not-allowed' : 'cursor-pointer'
+          )}
+        />
       </div>
     </div>
   );
